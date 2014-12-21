58b09f5b1c44cd478fd28d2c40b6a04509b7857c
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2014-12-21 16:44:50 -0700

    Solved P18

--- ProjectEuler/Controllers/ProblemsController.cs
+++ ProjectEuler/Controllers/ProblemsController.cs
@@ -386,6 +386,40 @@ namespace ProjectEuler.Controllers {
             ViewBag.Answer = iSum + "onethousand".Length;
             return View();
         }
+        public ActionResult Problem18() {
+            int[][] src = {
+                new int[] {75},
+                new int[] {95, 64},
+                new int[] {17, 47, 82},
+                new int[] {18, 35, 87, 10},
+                new int[] {20, 04, 82, 47, 65},
+                new int[] {19, 01, 23, 75, 03, 34},
+                new int[] {88, 02, 77, 73, 07, 63, 67},
+                new int[] {99, 65, 04, 28, 06, 16, 70, 92},
+                new int[] {41, 41, 26, 56, 83, 40, 80, 70, 33},
+                new int[] {41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+                new int[] {53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+                new int[] {70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+                new int[] {91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+                new int[] {63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+                new int[] {04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23}
+            };
+            int[,] solve = new int[src.Length,src.Length];
+            int end = 0;
+            int iMax = 0;
+            solve[0,0] = src[0][0];
+            for(int y=1;y<src.Length;y++) {
+                solve[y,0] = src[y][0]+solve[y-1,0];
+                end = src[y].Length-1;
+                solve[y,end] = src[y][end]+solve[y-1,end-1];
+                for(int x=1;x<src[y].Length-1;x++) {
+                    solve[y,x] = Math.Max(solve[y-1,x],solve[y-1,x-1]) + src[y][x];
+                }
+            }
+            for (int x = 0; x < src.Length; x++) iMax = Math.Max(solve[src.Length - 1, x], iMax);
+            ViewBag.Answer = iMax;
+            return View();
+        }
         public ActionResult ProblemN() {
             ViewBag.Answer = 0;
             return View();
