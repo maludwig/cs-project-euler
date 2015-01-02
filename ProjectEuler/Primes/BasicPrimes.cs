b047703827ec4f83efea561d1f70326427fcf345
MikePors <mike.pors@gmail.com>
2015-01-02 05:43:07 -0700

    Added BasicPrimes class

---
+++ ProjectEuler/Primes/BasicPrimes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Web;
+
+namespace ProjectEuler.Primes {
+    public class BasicPrimes : IPrimalityTest {
+        public bool IsPrime(int i) {
+            return IsPrime((long)i);
+        }
+        public bool IsPrime(long i) {
+            long divisor = 2;
+            if (i < 2) {
+                return false;
+            }
+            while (divisor < i) {
+                if (i % divisor == 0) {
+                    return false;
+                }
+                divisor++;
+            }
+            return true;
+        }
+        public bool IsPrime(BigInteger b) {
+            BigInteger divisor = 2;
+            if (b < 2) {
+                return false;
+            }
+            while (divisor < b) {
+                if (b % divisor == 0) {
+                    return false;
+                }
+                divisor++;
+            }
+            return true;
+
+        }
+        public bool IsComposite(int i) {
+            return !IsPrime(i);
+        }
+        public bool IsComposite(long i) {
+            return !IsPrime(i);
+        }
+        public bool IsComposite(BigInteger b) {
+            return !IsPrime(b);
+        }
+
+    }
+}
